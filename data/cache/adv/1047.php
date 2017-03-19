444027cb1465539ad5519f3f6c3505ef0b025206
root <root@localhost.localdomain>
2017-03-19 14:27:28 +0800

    first commit after installation

---
+++ data/cache/adv/1047.php
@@ -0,0 +1,31 @@
+<?php defined('ShopNC_CLUB') or exit('Access Invalid!'); return array (
+  'ap_id' => '1047',
+  'ap_name' => '首页顶部banner',
+  'ap_class' => '0',
+  'ap_display' => '2',
+  'is_use' => '1',
+  'ap_width' => '1200',
+  'ap_height' => '90',
+  'adv_num' => '1',
+  'click_num' => '0',
+  'default_content' => '05432451286887914.png',
+  'adv_list' => 
+  array (
+    0 => 
+    array (
+      'adv_id' => '936',
+      'ap_id' => '1047',
+      'adv_title' => '运维舫S7',
+      'adv_content' => 'a:2:{s:7:"adv_pic";s:0:"";s:11:"adv_pic_url";s:0:"";}',
+      'adv_start_date' => '1435766400',
+      'adv_end_date' => '1753891200',
+      'slide_sort' => '0',
+      'member_id' => '0',
+      'member_name' => '',
+      'click_num' => '0',
+      'is_allow' => '1',
+      'buy_style' => '',
+      'goldpay' => '0',
+    ),
+  ),
+);
---
+++ data/cache/nav.php
@@ -0,0 +1,79 @@
+<?php defined('ShopNC_CLUB') or exit('Access Invalid!'); return array (
+  0 => 
+  array (
+    'nav_id' => '9',
+    'nav_type' => '0',
+    'nav_title' => 'Join Us',
+    'nav_url' => 'http://www.coolandless.cc',
+    'nav_location' => '2',
+    'nav_new_open' => '0',
+    'nav_sort' => '210',
+    'item_id' => '0',
+  ),
+  1 => 
+  array (
+    'nav_id' => '8',
+    'nav_type' => '0',
+    'nav_title' => 'Partnership',
+    'nav_url' => 'http://www.coolandless.cc',
+    'nav_location' => '2',
+    'nav_new_open' => '0',
+    'nav_sort' => '220',
+    'item_id' => '0',
+  ),
+  2 => 
+  array (
+    'nav_id' => '7',
+    'nav_type' => '0',
+    'nav_title' => 'Contact Us',
+    'nav_url' => 'http://www.coolandless.cc',
+    'nav_location' => '2',
+    'nav_new_open' => '0',
+    'nav_sort' => '240',
+    'item_id' => '0',
+  ),
+  3 => 
+  array (
+    'nav_id' => '6',
+    'nav_type' => '0',
+    'nav_title' => 'About Us',
+    'nav_url' => 'http://www.coolandless.cc/http://s7.shopnc.club/member/index.php?act=article&amp;article_id=22',
+    'nav_location' => '2',
+    'nav_new_open' => '0',
+    'nav_sort' => '255',
+    'item_id' => '0',
+  ),
+  4 => 
+  array (
+    'nav_id' => '10',
+    'nav_type' => '0',
+    'nav_title' => 'Shop',
+    'nav_url' => 'http://www.coolandless.cc',
+    'nav_location' => '1',
+    'nav_new_open' => '0',
+    'nav_sort' => '255',
+    'item_id' => '0',
+  ),
+  5 => 
+  array (
+    'nav_id' => '11',
+    'nav_type' => '0',
+    'nav_title' => 'News',
+    'nav_url' => 'http://www.coolandless.cc/cms',
+    'nav_location' => '1',
+    'nav_new_open' => '1',
+    'nav_sort' => '255',
+    'item_id' => '0',
+  ),
+  6 => 
+  array (
+    'nav_id' => '16',
+    'nav_type' => '0',
+    'nav_title' => 'Links',
+    'nav_url' => 'http://www.coolandless.cc/index.php?act=link',
+    'nav_location' => '2',
+    'nav_new_open' => '0',
+    'nav_sort' => '255',
+    'item_id' => '0',
+  ),
+);
