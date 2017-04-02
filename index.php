f1df28269ae9d417ec92e78d7cd532301fc3fa86
carl <2207098687@qq.com>
2017-04-02 22:12:51 +0800

    gitignore update

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
  * 入口
  *
  *
- *
- * by 运维舫  www.shopnc.club
+ 
  */
 $site_url = strtolower('http://'.$_SERVER['HTTP_HOST'].substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/index.php')).'/shop/index.php');
 //@header('Location: '.$site_url);
